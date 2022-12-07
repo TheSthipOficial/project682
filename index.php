4e55504233a9b61fa8c0890179367af0bc72faec
TheSthipOficial <113964780+TheSthipOficial@users.noreply.github.com>
2022-12-07 03:55:26 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,61 +1,78 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple Chatbot in PHP | CodingNepal</title>
-    <link rel="stylesheet" href="https://thesthipoficial.github.io/project682\f.css">
-    <body bgcolor="#192739">
-    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-
-
-</head>
-<body>
-    <div class="wrapper">
-        <div class="title">Soul Super Bot</div>
-        <div class="form">
-            <div class="bot-inbox inbox">
-                <div class="icon">
-                    <i class="fas fa-user"></i>
-                </div>
-                <div class="msg-header">
-                    <p>Hola en que te puedo  Ayudar Hoy?</p>
-                </div>
-            </div>
-        </div>
-        <div class="typing-field">
-            <div class="input-data">
-                
-                <input id="data" type="text" placeholder="Escribe lo que deseas.." required>
-                <button id="send-btn">Enviar</button>
-            </div>
-        </div>
+<html lang="es">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>COPIAR</title>
+    <!-- Google Fonts -->
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  
+  <body>
+    <h1>!A DISFRUTAR¡</h1>
+     <h1>Cuenta Full Soul Khight </h1>
+    <div class="container">
+    <h3 style="color:">Correo</h3>
+      <input type="text" id="text-1" value="soulbotno@gmail.com" readonly/>
+      <button onclick="copy('text-1')">Copy </button>
     </div>
-
-    <script>
-        $(document).ready(function(){
-            $("#send-btn").on("click", function(){
-                $value = $("#data").val();
-                $msg = '<div class="user-inbox inbox"><div class="msg-header"><p>'+ $value +'</p></div></div>';
-                $(".form").append($msg);
-                $("#data").val('');
-                
-                // start ajax code
-                $.ajax({
-                    url: 'message.php',
-                    type: 'POST',
-                    data: 'text='+$value,
-                    success: function(result){
-                        $replay = '<div class="bot-inbox inbox"><div class="icon"><i class="fas fa-user"></i></div><div class="msg-header"><p>'+ result +'</p></div></div>';
-                        $(".form").append($replay);
-                        // when chat goes down the scroll bar automatically comes to the bottom
-                        $(".form").scrollTop($(".form")[0].scrollHeight);
-                    }
-                });
-            });
-        });
-    </script>
+    <div class="container">
+ <h3 style="color:">Contraseña</h3>
+      <input type="text" id="text-2" value="Soulbotnorobe" readonly />
+      <button onclick="copy('text-2')">Copy</button>
+    </div>
+ <br>
+     <h4>*SOLO COPIAR PROGRESO*</h4>
+     <h4>*PROHIBIDO CAMBIAR CONTRASEÑA*</h4>
+    <h4>*PROHIBIDO  SOBRESCRIBIR PROGRESO*</h4>
     
-</body>
+<style>
+  * {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  font-family: "Poppins", sans-serif;
+}
+body {
+  height: 100vh;
+  background: #192739; color:#fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+.container {
+  background-color: #192739;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 10px 0;
+}
+.container input {
+  font-size: 10px;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid #ffffff;
+}
+.container button {
+  font-size: 10px;
+  padding: 10px 20px;
+  background-color: #ff0000;
+  color: #ffffff;
+  border: #ffffff;;
+  border-radius: 5px;
+  margin-left: 10px;
+}
+</style>
+<script>
+  //Pass the id of the <input> element to be copied as a parameter to the copy()
+let copy = (textId) => {
+  //Selects the text in the <input> elemet
+  document.getElementById(textId).select();
+  //Copies the selected text to clipboard
+  document.execCommand("copy");
+};
+</script>
+  </body>
 </html>
